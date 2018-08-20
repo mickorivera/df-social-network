1a39d06784d013651e32b0d7ac43af16ab2dc912
mickorivera <18693715+mickorivera@users.noreply.github.com>
2018-08-20 18:47:01 +0800

    Update  model fields

--- feeds/models.py
+++ feeds/models.py
@@ -4,6 +4,6 @@ from accounts.models import User
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=1024)
     datetime_created = models.DateTimeField(auto_now_add=True)
