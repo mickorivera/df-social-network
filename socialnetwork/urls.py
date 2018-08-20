7ca487025703721ac4a1d0c0b3dd5e2daba56327
mickorivera <18693715+mickorivera@users.noreply.github.com>
2018-08-20 18:44:14 +0800

    Add  app for the posts

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/', include('accounts.urls')),
+    url(r'^api/v1/', include('feeds.urls')),
 ]
